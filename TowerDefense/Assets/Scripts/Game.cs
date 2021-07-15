259a9f30032545955f6c30da05a091d8c01766e8
kodlon <kodlon.igorchuchman@gmail.com>
2021-07-16 02:52:19 +0300

    Script main game init gameboard

---
+++ TowerDefense/Assets/Scripts/Game.cs
@@ -0,0 +1,18 @@
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2Int _boardSize;
+
+    [SerializeField]
+    private GameBoard _board;
+
+    private void Start()
+    {
+        _board.Initialize(_boardSize);
+    }
+}
